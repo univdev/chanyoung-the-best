580dc26ca247c74792301d15f067f316d3bff7ec
박찬영 <pcydeveloper@gmail.com>
2022-02-18 15:50:33 +0900

    feat: confetti 적용

--- src/routes/recommends/index.svelte
+++ src/routes/recommends/index.svelte
@@ -3,11 +3,13 @@
   import firebase from 'plugins/firebase';
   import { getFirestore, collection, query, where, orderBy, onSnapshot, addDoc } from 'firebase/firestore';
   import moment from 'moment';
+  import Confetti from 'canvas-confetti';
   import Recommend from "./.components/Recommend.svelte";
   import RecommendEditor from './.components/RecommendEditor.svelte';
   import ConfirmModal from './.components/ConfirmModal.svelte';
   import CompleteModal from './.components/CompleteModal.svelte';
 
+  let confettiCanvas = null;
   let recommends = [];
   let recommendsSubscriber = null;
   let isVisibleRecommendEditor = false;
@@ -77,12 +79,49 @@
     isVisibleCompleteModal = false;
     handleHideConfirmModal();
     handleHideRecommendEditor();
+    showCompleteEffect(confettiCanvas, new Date());
+  };
+  const createConfettiCanvas = () => {
+    const canvas = document.createElement('canvas');
+    canvas.setAttribute('class', 'confetti-canvas');
+    document.body.appendChild(canvas);
+    const confetti = Confetti.create(canvas, {
+      resize: true,
+      useWorker: true,
+    });
+    return confetti;
+  };
+  const destroyConfettiCanvas = () => {
+    document.querySelector('canvas').remove();
+  };
+  const showCompleteEffect = (confetti, startTime, duration = 3) => {
+    if (!confetti) throw new Error('Confetti Canvas를 정의해주세요.');
+    const now = moment().toDate();
+    const endTime = moment(startTime).add(duration, 'seconds').toDate();
+    confetti({
+      particleCount: 2,
+      angle: 60,
+      spread: 55,
+      origin: { x: 0 },
+    });
+    confetti({
+      particleCount: 2,
+      angle: 120,
+      spread: 55,
+      origin: { x: 1 },
+    });
+    window.setTimeout(() => {
+      console.log(startTime);
+      if (now <= endTime) showCompleteEffect(confetti, startTime, duration);
+    }, 10);
   };
   onMount(() => {
+    confettiCanvas = createConfettiCanvas();
     recommendsSubscriber = getRecommendsSubscriber();
   });
   onDestroy(() => {
     if (recommendsSubscriber) recommendsSubscriber();
+    if (confettiCanvas) destroyConfettiCanvas();
   });
 </script>
 
@@ -146,5 +185,13 @@
     border-radius: 50%;
     width: 50px;
     height: 50px;
+  }
+  :global(.confetti-canvas) {
+    position: fixed;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    z-index: -1;
   }
 </style>
