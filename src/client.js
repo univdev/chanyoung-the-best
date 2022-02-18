f65e267ff587377df1273399e2a5527a54c579d7
박찬영 <pcydeveloper@gmail.com>
2022-02-18 14:31:08 +0900

    chore: Firestore 적용

--- src/client.js
+++ src/client.js
@@ -1,6 +1,7 @@
 import * as sapper from '@sapper/app';
 import 'papercss/dist/paper.min.css';
 import 'assets/css/common.css';
+import 'plugins/firebase-config';
 
 sapper.start({
 	target: document.body,
